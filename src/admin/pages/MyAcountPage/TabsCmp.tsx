f1e7533b8256873cb6201882abcc17307d10c4ed
Derkysan <derkysan19@gmail.com>
2023-12-05 11:29:09 -0300

    fix build error

--- src/admin/pages/MyAcountPage/TabsCmp.tsx
+++ src/admin/pages/MyAcountPage/TabsCmp.tsx
@@ -43,6 +43,7 @@ export default function TabsCmp() {
   const [value, setValue] = React.useState(0);
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    console.log(event);
     setValue(newValue);
   };
 
