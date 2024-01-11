1c5b48c614cfa77d6b1cfa7c0e89bce0b346b0d5
Derkysan <derkysan19@gmail.com>
2024-01-11 13:17:19 -0300

    add general modal response

--- src/web/pages/Landing.tsx
+++ src/web/pages/Landing.tsx
@@ -5,10 +5,19 @@ import { HowItWorks } from "../components/HowItWorks"
 import { Pricing } from "../components/Pricing"
 import { Box } from "@mui/material"
 import { Simulator } from "../components/Simulator"
+import { ResponseModal } from "src/components/ResponseModal"
+import { useState } from "react"
 
 export const Landing = () => {
+  const [isOpenResponseModal, setIsOpenResponseModal] = useState(true);
   return (
     <Box sx={{ overflowX: 'hidden' }} className="animate__animated animate__fadeIn">
+      <ResponseModal 
+        isOpen={isOpenResponseModal} 
+        isError
+        title="Mi modal" 
+        onClose={() => setIsOpenResponseModal(false)} 
+      />
       <MainBanner />
       <Benefits />
       <CallToAction />
